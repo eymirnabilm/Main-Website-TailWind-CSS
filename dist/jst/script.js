70e009466f2953fae43146a6c87611397742e85f
Eymir Nabil Makarim <105166271+eymirnabilm@users.noreply.github.com>
2022-08-30 23:33:24 +0700

    Add files via upload

---
+++ dist/jst/script.js
@@ -0,0 +1,25 @@
+//navbar fixed
+window.onscroll = function () {
+    const header = document.querySelector("header");
+    const fixedNav = header.offsetTop;
+    const toTop = document.querySelector("#to-top");
+  
+    if (window.pageYOffset > fixedNav) {
+      header.classList.add("navbar-fixed");
+      toTop.classList.remove("hidden");
+      toTop.classList.add("flex");
+    } else {
+      header.classList.remove("navbar-fixed");
+      toTop.classList.remove("flex");
+      toTop.classList.add("hidden");
+    }
+  };
+  
+  // Menu
+  const menu = document.querySelector("#menu");
+  const navMenu = document.querySelector("#nav-menu");
+  
+  menu.addEventListener("click", function () {
+    menu.classList.toggle("menu-active");
+    navMenu.classList.toggle("hidden");
+  });
